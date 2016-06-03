94daf43fec6061ea05f5a3449e772427e5f5c40b
Modanung <frode@lindeijer.nl>
2016-06-03 23:11:15 +0200

    Make sure script starts in same directory as script

--- CloneMakeUrho3D.sh
+++ CloneMakeUrho3D.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-sudo apt-get install libx11-dev libxrandr-dev libasound2-dev git cmake make
+sudo apt-get install libx11-dev libxrandr-dev libasound2-dev git cmake make;
 
+cd `dirname $0`;
 if [ ! -d Urho3D ]
 then
   git clone https://github.com/Urho3D/Urho3D
