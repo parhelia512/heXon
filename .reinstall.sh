9c196a40c208c7fac34131e63a98b3fba748c95f
Frode Lindeijer <frode@lindeijer.nl>
2016-06-06 04:45:29 +0200

    hid script

--- .reinstall.sh
+++ .reinstall.sh
@@ -1,26 +1,19 @@
 sudo apt-get install libx11-dev libxrandr-dev libasound2-dev git cmake make
 
 cd `dirname $0`
-if [ ! -e Urho3D ]
+if [ ! -d Urho3D ]
 then
-	cd ..
-	if [ ! -d Urho3D ]
-	then
-  	git clone https://github.com/Urho3D/Urho3D
-	fi
-	cd `dirname $0`
-	ln -s ../Urho3D
+  git clone https://github.com/Urho3D/Urho3D
 fi
 
 cd Urho3D
 git pull
+./cmake_clean.sh
 ./cmake_generic.sh . -URHO3D_ANGELSCRIPT=0 -URHO3D_NAVIGATION=0 -URHO3D_URHO2D=0 -URHO3D_SAMPLES=0 -URHO3D_TOOLS=0
 make
-cd `dirname $0`
+cd ..
 
 git pull
 qmake heXon.pro
-make
-sudo make uninstall
 sudo make install
 sudo update-icon-caches ~/.local/share/icons/
